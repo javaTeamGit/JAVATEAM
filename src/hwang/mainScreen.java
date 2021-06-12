6df9ae6981dfe014e9937ee45d476eee9dde2ce0
admin <admin@DESKTOP-H89799E>
2021-06-12 15:42:17 +0900

    진행중

--- src/hwang/mainScreen.java
+++ src/hwang/mainScreen.java
@@ -7,6 +7,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
@@ -48,7 +49,7 @@ public class mainScreen extends JFrame implements ActionListener{
 		JLabel lbl1 = new JLabel("독서실 인하공전점");
 		lbl1.setFont(new Font("맑은 고딕",Font.BOLD,18));
 		lbl1.setHorizontalAlignment(JLabel.CENTER);
-		id = new JLabel("hydra");
+		id = new JLabel(title);
 		id.setHorizontalAlignment(JLabel.CENTER);
 		id.setFont(new Font("맑은 고딕",Font.PLAIN,14));
 		p1.add(sp1);
@@ -130,38 +131,51 @@ public class mainScreen extends JFrame implements ActionListener{
 			ss=new startScreen("",300,230);
 			dispose();
 		}else if(obj==btnRoom) {
-			String sql = "SELECT * FROM TIME WHERE CUSTID ='" + id.getText() + "' AND SEATID IS NOT NULL";
-			ResultSet rs = db.JDBC.getResultSet(sql);
-			System.out.println(sql);
+			String sq="SELECT * FROM TIME WHERE CUSTID = '"+id.getText()+"' AND RMTIME>0";
+			ResultSet r= db.JDBC.getResultSet(sq);
 			try {
-				if(rs.next()) { //이미 좌석에 앉아있었을 경우
-					int result = JOptionPane.showConfirmDialog(null, "퇴실하시겠습니까?","퇴실",JOptionPane.YES_NO_OPTION);
-					if(result == JOptionPane.YES_OPTION) {
-						String sql1 = "UPDATE TIME SET SEATID = NULL  WHERE CUSTID= '"+id.getText()+"'";
-						db.JDBC.executeQuery(sql1);
-						Timestamp ts = new Timestamp(System.currentTimeMillis());
-						String sq1 = "UPDATE TIME SET EXIT = '"+ts+"'  WHERE CUSTID= '" +id.getText()+ "'";
-						db.JDBC.executeQuery(sq1);
-						System.out.println(sq1);
-						String sql2 = "SELECT ENTRANCE FROM TIME WHERE CUSTID= '"+id.getText()+"'";
-						ResultSet rs1 =db.JDBC.getResultSet(sql2);
-						String sql3 = "SELECT EXIT FROM TIME WHERE CUSTID= '"+id.getText()+"'";
-						ResultSet rs2 =db.JDBC.getResultSet(sql3);
-						if(rs1.next()&&rs2.next()) {
-							Timestamp t1 = rs1.getTimestamp("ENTRANCE");
-							Timestamp t2 = rs2.getTimestamp("EXIT");
-							long diff = t2.getTime() - t1.getTime();
-							long diffs =diff/1000;
-							System.out.println(diffs);
-						
-						}else {
+				if(r.next()) { //남은 시간이 0 이상이면 좌석 이용 가능
+					String sql = "SELECT * FROM TIME WHERE CUSTID ='" + id.getText() + "' AND SEATID IS NOT NULL";
+					ResultSet rs = db.JDBC.getResultSet(sql);
+					System.out.println(sql);
+					try {
+						if(rs.next()) { //이미 좌석에 앉아있었을 경우
+							int result = JOptionPane.showConfirmDialog(null, "퇴실하시겠습니까?","퇴실",JOptionPane.YES_NO_OPTION);
+							if(result == JOptionPane.YES_OPTION) {
+								String sql1 = "UPDATE TIME SET SEATID = NULL  WHERE CUSTID= '"+id.getText()+"'"; // 퇴실 선택하면 좌석번호 없앰
+								db.JDBC.executeQuery(sql1);
+								Timestamp ts = new Timestamp(System.currentTimeMillis());
+								String sq1 = "UPDATE TIME SET EXIT = '"+ts+"'  WHERE CUSTID= '" +id.getText()+ "'"; // 퇴실 시간 저장
+								db.JDBC.executeQuery(sq1);
+								System.out.println(sq1);
+								String sql2 = "SELECT ENTRANCE FROM TIME WHERE CUSTID= '"+id.getText()+"'";
+								ResultSet rs1 =db.JDBC.getResultSet(sql2);
+								String sql3 = "SELECT EXIT FROM TIME WHERE CUSTID= '"+id.getText()+"'";
+								ResultSet rs2 =db.JDBC.getResultSet(sql3);
+								if(rs1.next()&&rs2.next()) { //사용시간 계산 (초 기준)
+									Timestamp t1 = rs1.getTimestamp("ENTRANCE");
+									Timestamp t2 = rs2.getTimestamp("EXIT");
+									long diff = t2.getTime() - t1.getTime();
+									long diffs =diff/1000;
+									System.out.println(diffs);	
+								}else {
+								}
+							}
+						}else { //입실하는 경우 좌석선택 화면 열기
+							sr=new studyRoom(id.getText(),500,480);
+							dispose();
 						}
+					} catch (SQLException e1) {
+						e1.printStackTrace();
 					}
-				}else { //입실하는 경우
-					sr=new studyRoom(id.getText(),500,480);
-					dispose();
+				}else { //남은시간이 0이하면 이용권 결제 안내
+					JOptionPane.showMessageDialog(this, "이용권을 결제하세요.","안내",JOptionPane.PLAIN_MESSAGE);;
 				}
+			} catch (HeadlessException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 		}else if(obj==btnLogout) {
